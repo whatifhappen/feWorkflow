874bde3caa8f259225a58b691b16f947d85455d0
whatifhappen <treong@gmail.com>
2016-04-04 20:06:42 +0800

    测试redux

--- src/colors.js
+++ src/colors.js
@@ -1,2 +1,58 @@
-export const NICE = 'skyblue';
-export const SUPER_NICE = 'darkred';
+import Colors from 'material-ui/lib/styles/colors';
+import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
+import Spacing from 'material-ui/lib/styles/spacing';
+import zIndex from 'material-ui/lib/styles/zIndex';
+import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import MyRawTheme from 'path/to/your/raw/theme/file';
+
+const MySampleAppComponent = React.createClass({
+
+  //the key passed through context must be called "muiTheme"
+  childContextTypes : {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(MyRawTheme),
+    };
+  },
+
+  //the app bar and button will receive our theme through
+  //context and style accordingly
+  render () {
+    return (
+      <div>
+        <AppBar title="My AppBar" />
+        <RaisedButton label="My Button" primary={true} />
+      </div>
+    );
+  },
+});
+
+export default MySampleAppComponent;
+
+
+export default {
+  spacing: Spacing,
+  zIndex: zIndex,
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: Colors.cyan500,
+    primary2Color: Colors.cyan700,
+    primary3Color: Colors.lightBlack,
+    accent1Color: Colors.pinkA200,
+    accent2Color: Colors.grey100,
+    accent3Color: Colors.grey500,
+    textColor: Colors.darkBlack,
+    alternateTextColor: Colors.white,
+    canvasColor: Colors.white,
+    borderColor: Colors.grey300,
+    disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
+    pickerHeaderColor: Colors.cyan500,
+  }
+};
